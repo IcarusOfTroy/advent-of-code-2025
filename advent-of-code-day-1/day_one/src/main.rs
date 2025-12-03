1d17bb6101eae5fbea015e4d009ca15ef527fbf5
Matthew Richmond <matthew.richmond@wegmans.com>
2025-12-03 00:52:24 -0500

    push for solving day one part 2

--- advent-of-code-day-1/day_one/src/main.rs
+++ advent-of-code-day-1/day_one/src/main.rs
@@ -9,19 +9,24 @@ fn main() {
     let line_file_reader = BufReader::new(File::open(&input_file).expect(&format!("Cannot open {}", path)));
 
     let mut dial = 50;
-    let mut zero_count = 0;
+    let mut times_zero_is_clicked = 0;
 
     for line in line_file_reader.lines() {
         print!("Current dial position: {} ", dial);
-        let instruction = parse_instruction(&line.unwrap());
-        print!("Instruction: {}{}, ", instruction.0, instruction.1);
-        dial = turn_dial(dial, &instruction.0, instruction.1);
-        if dial == 0 {
-            zero_count += 1;
-        }
+        let instruction: (String, i32) = parse_instruction(&line.unwrap());
+        
+        let step = match instruction.0.as_str() {
+            "L" => -(instruction.1),
+            "R" => instruction.1,
+            _ => 0,
+        };
+
+        let result_of_instruction = turn_dial_one_click_at_a_time_counting_zeros(dial, step);
+        dial = result_of_instruction.0;
+        times_zero_is_clicked += result_of_instruction.1;
         println!("New dial position: {}", dial);
     }
-    println!("Zero count: {}", zero_count);
+    println!("Zero count: {}", times_zero_is_clicked);
 }
 
 fn parse_instruction(instruction: &str) -> (String, i32) {
@@ -30,26 +35,24 @@ fn parse_instruction(instruction: &str) -> (String, i32) {
     (direction.to_string(), steps)
 }
 
-fn turn_dial(current_position: i32, direction: &str, steps: i32) -> i32 {
-    let sanitized_steps = steps % 100;
-    let new_position = match direction {
-        "L" => {
-            let next = current_position - sanitized_steps;
-            if next < 0 {
-                100 + next
-            } else {
-                next
-            }
-       }
-        "R" => {
-            let next = current_position + sanitized_steps;
-            if next > 99 {
-                next - 100
-            } else {
-                next
-            }
+fn turn_dial_one_click_at_a_time_counting_zeros(current_position: i32, steps: i32) -> (i32, i32) {
+    let mut position = current_position;
+    let mut zero_count = 0;
+
+    let step_direction = if steps > 0 { 1 } else { -1 };
+    for _ in 0..steps.abs() {
+        position += step_direction;
+        if position > 99 {
+            position = 0;
         }
-        _ => current_position,
-    };
-    new_position
+        else if position < 0 {
+            position = 99;
+        }
+
+        if position == 0 {
+            zero_count += 1;
+        }
+    }
+
+    (position, zero_count)
 }
